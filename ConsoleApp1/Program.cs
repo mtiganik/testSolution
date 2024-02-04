7c4a83d637c1ad0c1f1ec5f49a161196109c5b40
Mihkel Tiganik <mtiganik@gmail.com>
2024-02-04 21:08:55 +0200

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Hello, World!");
+PrintManyLines();
+void PrintManyLines()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
